d37970aa8d8c34ab60ca2374679730e77c0d5a90
Maxim Balan <balan.maxim@gmail.com>
2018-11-13 16:25:07 +0000

    adding demo template

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,23 @@
+
+
+import config.TestConfig;
+import io.dropwizard.Configuration;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class AppConfig extends Configuration {
+    
+    @Valid
+    @NotNull
+    @JsonProperty("configTest")
+    private TestConfig testConfig;
+    
+    public TestConfig getTestConfig() {
+        return this.testConfig;
+    }
+    
+}
---
+++ src/main/java/resources/Greeting.java
@@ -0,0 +1,19 @@
+package resources;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class Greeting {
+    
+    private final String name;
+    
+    public Greeting(String name) {
+        this.name = name;
+    }
+    
+    @JsonProperty("greeting")
+    public String greetingMessage() {
+        return String.format("Hello %s", this.name);
+    }
+    
+}
